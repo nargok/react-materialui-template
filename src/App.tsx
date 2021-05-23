5a60911c8eaffd7d7eeebc423218cca7d4ede46a
nargok <nargok@gmail.com>
2021-05-23 22:08:39 +0900

    GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from '@material-ui/core'
 import { initialStore, Provider } from './store/'
 import ListRefreshControl from './components/ListRefreshControl'
 import CountryList from './components/CountryList'
+import GlobalStyles from './components/GlobalStyles'
 
 import theme from './theme'
 
@@ -13,6 +14,7 @@ function App() {
   return (
     <Provider value={store}>
       <ThemeProvider theme={theme}>
+        <GlobalStyles />
         <div className="App">
           <h3>React UI template project</h3>
         </div>
